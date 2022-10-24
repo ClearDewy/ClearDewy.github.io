087bedfd8794735a5d30af35089b7b6f0e250894
Qingtuan <2233408834@qq.com>
2022-10-24 23:37:52 +0800

    First Commit

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,14 @@
 import { defineUserConfig } from "vuepress";
-// import {searchPlugin} from "@vuepress/plugin-search"
+import {searchPlugin} from "@vuepress/plugin-search"
 
-import theme from "./theme.js";
+import hopeTheme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
   lang: "zh-CN",
   title: "Dewy-Wiki",
   description: "ClearDewy's own Wiki",
-
-  theme,
+  theme:hopeTheme,
 
   // plugins:[
   //   searchPlugin({
