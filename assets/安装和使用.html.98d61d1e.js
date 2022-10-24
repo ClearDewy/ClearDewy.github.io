794a11febdbbceb8798e1a50cc1f153145c8de09
ClearDewy <93588007+ClearDewy@users.noreply.github.com>
2022-10-24 12:11:59 +0000

    Deploying to gh-pages from @ ClearDewy/ClearDewy.github.io@7f228cecef05f4af7d2de75b9228d2fa7da7b354 🚀

---
+++ assets/安装和使用.html.98d61d1e.js
@@ -0,0 +1,9 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as n,c as s,d as a}from"./app.4d3b7776.js";const i={},l=a(`<h1 id="\u5B89\u88C5\u548C\u4F7F\u7528" tabindex="-1"><a class="header-anchor" href="#\u5B89\u88C5\u548C\u4F7F\u7528" aria-hidden="true">#</a> \u5B89\u88C5\u548C\u4F7F\u7528</h1><div class="language-powershell ext-powershell line-numbers-mode"><pre class="language-powershell"><code><span class="token comment"># \u4F7F\u7528Webstrom\u5B89\u88C5vite\u63D2\u4EF6\u540E\u6784\u5EFA\u9879\u76EE\uFF0C\u9009\u62E9vue-ts</span>
+
+<span class="token comment"># \u8FD0\u884C</span>
+npm run dev
+
+<span class="token comment">#\u6253\u5305</span>
+npm vite build
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u6784\u5EFA\u9879\u76EE" tabindex="-1"><a class="header-anchor" href="#\u6784\u5EFA\u9879\u76EE" aria-hidden="true">#</a> \u6784\u5EFA\u9879\u76EE</h2><ul><li>\u5B89\u88C5Element-plus</li></ul><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code>npm install element<span class="token operator">-</span>plus <span class="token operator">--</span>save
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,5),r=[l];function d(c,t){return n(),s("div",null,r)}const v=e(i,[["render",d],["__file","\u5B89\u88C5\u548C\u4F7F\u7528.html.vue"]]);export{v as default};
